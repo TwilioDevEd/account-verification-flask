fc6db503809e3e350081637ccb7b0f7e8eb67619
nebtrx <neb.trx@gmail.com>
2015-11-20 17:46:49 -0500

    Switch the default config to use in memory sqlite

--- account_verification_flask/config/local.py
+++ account_verification_flask/config/local.py
@@ -5,5 +5,5 @@ TWILIO_ACCOUNT_SID = 'your_twilio_account_sid'
 TWILIO_AUTH_TOKEN = 'your_twilio_auth_token'
 TWILIO_NUMBER = 'your_twilio_phone_number'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:////Work/account_verification.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite://'
 SQLALCHEMY_ECHO = True
