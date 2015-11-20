9967bfea3a35aa21c060b22bb85943b29eb0a9c0
nebtrx <neb.trx@gmail.com>
2015-11-20 17:43:20 -0500

    Add local config file

---
+++ account_verification_flask/config/local.py
@@ -0,0 +1,9 @@
+﻿SECRET_KEY = '%^!@@*!&$8xdfdirunb52438#(&^874@#^&*($@*(@&^@)(&*)Y_)((+'
+AUTHY_KEY = 'your_authy_key'
+
+TWILIO_ACCOUNT_SID = 'your_twilio_account_sid'
+TWILIO_AUTH_TOKEN = 'your_twilio_auth_token'
+TWILIO_NUMBER = 'your_twilio_phone_number'
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:////Work/account_verification.db'
+SQLALCHEMY_ECHO = True
